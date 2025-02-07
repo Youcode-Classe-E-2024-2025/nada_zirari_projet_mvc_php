bbd1bda40c82537f9278b2938375caaf2dc2ef24
Nada Zirari <zirarinada20@gmail.com>
2025-02-08 00:01:57 +0100

    final modifications

--- app/controllers/front/ArticleController.php
+++ app/controllers/front/ArticleController.php
@@ -1,4 +1,6 @@
 <?php
+
+ 
 namespace App\Controllers\Front;
 
 use App\Core\Controller;
@@ -6,10 +8,28 @@ use App\Models\Article;
 
 class ArticleController extends Controller
 {
+    // Afficher tous les articles
+    public function index()
+    {
+        // Créer une instance du modèle Article
+        $articleModel = new Article();
+        $articles = $articleModel->findAll(); // Récupère tous les articles
+
+        // Utiliser la méthode render() pour afficher la vue des articles
+        $this->render('front/article', ['article' => $articles]);
+    }
+
+    // Afficher un article spécifique par son ID
     public function show($id)
     {
-        $article = new Article();
-        $data = $article->getArticle($id);
-        $this->view('front/article', ['article' => $data]);
+        // Créer une instance du modèle Article
+        $articleModel = new Article();
+        $article = $articleModel->find($id); // Récupère un article par son ID
+
+        // Utiliser la méthode render() pour afficher la vue de l'article
+        $this->render('front/article', ['article' => $article]);
     }
 }
+
+
+?>
